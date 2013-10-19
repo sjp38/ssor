bd5f743fed41e433a60083d841b0b8c9ae2582f3
SeongJae Park <sj38.park@gmail.com>
2013-10-19 20:58:42 +0900

    Regist basic handlers for basic interfaces
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- main.go
+++ main.go
@@ -7,8 +7,23 @@ import (
 
 func init() {
     http.HandleFunc("/", welcome)
+    http.HandleFunc("/Signup", signupHandler)
+    http.HandleFunc("/Collector", collectorHandler)
+    http.HandleFunc("/Rune", runeHandler)
 }
 
 func welcome(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "<h1>Welcome to SOR</h1>")
+}
+
+func signupHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "signup handler")
+}
+
+func collectorHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "collector handler")
+}
+
+func runeHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "rune handler")
 }
