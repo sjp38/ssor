b02f1ce0fb2867956341aae4c88f5859b5739e1c
SeongJae Park <sj38.park@gmail.com>
2013-10-21 23:45:38 +0900

    Add basic models
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ models.go
@@ -0,0 +1,19 @@
+package sor
+
+import (
+)
+
+type Signup struct {
+    GoogleId int `json:"googleId"`
+}
+
+type Collector struct {
+    GoogleId int `json:"googleId"`
+    Nickname string `json:"nickname"`
+    Hp int `json:"hp"`
+    Mp int `json:"mp"`
+    Atk int `json:"atk"`
+    Def int `json:"def"`
+    Int int `json:"int"`
+    HunterClass string `json:"hunterClass"`
+}
