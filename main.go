7a5a339735680011e5127857165c5eb2f8bfa5b9
SeongJae Park <sj38.park@gmail.com>
2013-10-22 15:58:07 +0900

    Remove deprecated feature
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- main.go
+++ main.go
@@ -9,20 +9,12 @@ import (
 
 func init() {
     http.HandleFunc("/", welcome)
-    http.HandleFunc("/signup", signupHandler)
     http.HandleFunc("/collector", collectorHandler)
     http.HandleFunc("/rune", runeHandler)
 }
 
 func welcome(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "<h1>Welcome to SOR</h1>")
-}
-
-func signupHandler(w http.ResponseWriter, r *http.Request) {
-    defer r.Body.Close()
-    body, _ := ioutil.ReadAll(r.Body)
-    fmt.Fprintf(w, "Signup handler called. method: %s, body: %s",
-            r.Method, body)
 }
 
 func collectorHandler(w http.ResponseWriter, r *http.Request) {
