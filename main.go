3cad6c255e5a25c43c479a08cad2fca992b30348
SeongJae Park <sj38.park@gmail.com>
2013-10-22 18:34:45 +0900

    /collector POST: Seperate code to other function
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func welcome(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "<h1>Welcome to SOR</h1>")
 }
 
-func createCollector(collector Collector, c appengine.Context) bool {
+func insertCollector(collector Collector, c appengine.Context) bool {
     encKey := datastore.NewKey(c, "collector",
             "", int64(collector.GoogleId), nil)
     _, err := datastore.Put(c, encKey, &collector)
@@ -44,29 +44,34 @@ func getCollector(id int, c appengine.Context) (*Collector, bool) {
     return collector, true
 }
 
-func collectorHandler(w http.ResponseWriter, r *http.Request) {
+func createCollector(w http.ResponseWriter, r *http.Request) {
     c := appengine.NewContext(r)
     defer r.Body.Close()
     body, _ := ioutil.ReadAll(r.Body)
     var collector Collector
+    json.Unmarshal(body, &collector)
+    result := insertCollector(collector, c)
+    var resp Result
+    if result {
+        resp.Success = "success"
+    } else {
+        resp.Success = "fail"
+    }
+    dat, err := json.Marshal(resp)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+    fmt.Fprint(w, string(dat))
+}
+
+func collectorHandler(w http.ResponseWriter, r *http.Request) {
     switch r.Method {
     case "POST":
-        json.Unmarshal(body, &collector)
-        result := createCollector(collector, c)
-        var resp Result
-        if result {
-            resp.Success = "success"
-        } else {
-            resp.Success = "fail"
-        }
-        dat, err := json.Marshal(resp)
-        if err != nil {
-            log.Println(err)
-            return
-        }
-        fmt.Fprint(w, string(dat))
+        createCollector(w, r)
     case "PUT":
     case "GET":
+        c := appengine.NewContext(r)
         id, _ := strconv.Atoi(r.URL.Query()["googleId"][0])
         collector, succeed := getCollector(id, c)
         if false == succeed {
